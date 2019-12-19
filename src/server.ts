e7e71a07e3692ae998ad84ff09616d65412567c8
Alex Marchant <alexjmarchant@gmail.com>
2019-12-18 21:35:58 -0600

    Some small tweaks

--- src/server.ts
+++ src/server.ts
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
   res.sendStatus(200)
 })
 
-const port = 3001
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+const port = process.env.PORT || 3001
+app.listen(port)
