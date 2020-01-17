364103ec8ca12cf776b75712c1488f198823643d
Alex Marchant <alexjmarchant@gmail.com>
2020-01-17 00:10:16 -0600

    WIP auth

--- src/db/user.ts
+++ src/db/user.ts
@@ -1,13 +1,13 @@
-import { Model, DataTypes } from "sequelize";
-import { sequelize } from "./init";
+import { Model, DataTypes } from 'sequelize'
+import { sequelize } from './init'
 
 export class User extends Model {
-  id!: number;
-  name!: string;
-  email!: string;
-  password?: string;
-  readonly createdAt!: Date;
-  readonly updatedAt!: Date;
+  id!: number
+  name!: string
+  email!: string
+  hashedPassword!: string
+  readonly createdAt!: Date
+  readonly updatedAt!: Date
 }
 
 User.init(
@@ -15,23 +15,23 @@ User.init(
     id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
-      primaryKey: true
+      primaryKey: true,
     },
     name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
-    password: {
+    hashedPassword: {
       type: DataTypes.STRING,
-      allowNull: false
-    }
+      allowNull: false,
+    },
   },
   {
     sequelize,
-    modelName: "user"
-  }
-);
+    modelName: 'user',
+  },
+)
--- src/grapqhl/user/resolvers.ts
+++ src/grapqhl/user/resolvers.ts
@@ -1,12 +1,15 @@
-import { User, Category, Todo } from "../../db"
-import { Context } from "../../interfaces"
+import bcrypt from 'bcrypt'
+import { User } from '../../db'
+import { Context } from '../../interfaces'
+
+const saltRounds = 10
 
 export default {
   Query: {
     me(parent: {}, args: {}, context: Context): User {
       const { user } = context
       if (!user) {
-        throw new Error("Not logged in")
+        throw new Error('Not logged in')
       }
       return user
     },
@@ -14,49 +17,49 @@ export default {
     async users(parent: {}, args: {}, context: Context): Promise<User[]> {
       const users = await User.findAll()
       if (!users) {
-        throw new Error("There are no users")
+        throw new Error('There are no users')
       }
       return users
-    }
+    },
   },
   Mutation: {
     async login(
       parent: {},
-      args: { name?: string; email?: string; password?: string }
+      args: { email?: string; password?: string },
     ): Promise<string> {
       if (!args.email || !args.password) {
-        throw new Error("Missing required args")
+        throw new Error('Missing required args')
       }
       const user = await User.findOne({ where: { email: args.email } })
       if (!user) {
-        throw new Error("No user with that email")
+        throw new Error('No user with that email')
       }
-      if (user.password !== args.password) {
-        throw new Error("Invalid password")
+      if (!(await bcrypt.compare(args.password, user.hashedPassword))) {
+        throw new Error('Invalid password')
       }
-      return "1234"
+      return '1234'
     },
 
     async signup(
       parent: {},
-      args: { name?: string; email?: string; password?: string }
+      args: { name?: string; email?: string; password?: string },
     ): Promise<string> {
       const { name, email, password } = args
       if (!name || !email || !password) {
-        throw new Error("Missing required args")
+        throw new Error('Missing required args')
       }
       const existingUser = await User.findOne({
-        where: { email: email }
+        where: { email: email },
       })
       if (existingUser) {
-        throw new Error("User with that email exists")
+        throw new Error('User with that email exists')
       }
       const user = new User()
       user.name = name
       user.email = email
-      user.password = password
+      user.hashedPassword = await bcrypt.hash(args.password, saltRounds)
       await user.save()
-      return "1234"
-    }
-  }
+      return '1234'
+    },
+  },
 }
