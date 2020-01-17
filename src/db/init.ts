81a5d6fdc6b41aca7df28116d6565510b092e6ad
Alex Marchant <alexjmarchant@gmail.com>
2020-01-17 10:18:06 -0600

    Hide pg url

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,13 +1,13 @@
-import { Sequelize } from "sequelize"
+import { Sequelize } from 'sequelize'
 
 let pgurl
-if (process.env.NODE_ENV === "production") {
-  pgurl =
-    "postgres://sdcgblyluaoihh:530a88a2b6c301a3492e98903b4ad479f2e1d49b72d035ece848dd517553f2d0@ec2-174-129-234-111.compute-1.amazonaws.com:5432/d8d1qip7fctfv"
+if (process.env.NODE_ENV === 'production') {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Missing DATABASE_URL env var')
+  }
+  pgurl = process.env.DATABASE_URL
 } else {
-  pgurl = "postgres://localhost:5432/oscars2020"
+  pgurl = 'postgres://localhost:5432/oscars2020'
 }
 
 export const sequelize = new Sequelize(pgurl)
-
-// var sequelize = new Sequelize("postgres://user:pass@example.com:5432/dbname")
